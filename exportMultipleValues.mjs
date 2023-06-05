498424601e196dadd6df92eb3d324d0c29174f17
RanjithHNRK <hnranjithkumar@gmail.com>
2023-06-05 14:31:15 +0000

    MJS multiple files extraction

---
+++ exportMultipleValues.mjs
@@ -0,0 +1,12 @@
+const myArray = ["camel", 265, true, "5.6"];
+
+const bulb = {
+  watts: 10,
+  type: "LED",
+};
+
+const multiplyByFour = function (number) {
+  return number * 4;
+};
+
+export { myArray, bulb, multiplyByFour };
